88d54c330420aa6d6a31d7df2b41ff9870cf7c0a
Tuukka Heinonen <[tukkis@luukku.com]>
2020-09-02 13:49:17 +0300

    part 2

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import noteService from './services/people'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
 
 const App = () => {
   const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber , setNewNumber] = useState('')
   const [ filter , setFilter ] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    noteService
+      .getAll()
+      .then(initial => {
+        setPersons(initial)
       })
   }, [])
 
@@ -36,28 +38,91 @@ const App = () => {
       let newNumtoAdd = newNumber.split('').reduce((a,b) => !b.match(/[a-z]/g) ? a + b : a, '')
       let newNumRF = newNumber.split('').reduce((a,b) => !isNaN(b) ? a + b : a, '')
       let numbersRF = persons.map(person => person.number.split('').reduce((a,b) => !isNaN(b) ? a + b : a, ''))
-      let newNameTL = newName.toLowerCase()
-      let personsTL = persons.map(person => person.name.toLowerCase())
+      let newNameTL = newName.toLowerCase().replace(/\s/g, '')
+      let personsTL = persons.map(person => person.name.toLowerCase().replace(/\s/g, ''))
       if (personsTL.indexOf(newNameTL) > -1 || numbersRF.indexOf(newNumRF) > -1) {
-        alert(`${newName} is already added to phonebook`)
-      } else {const nameToAdd = {
-        name : newName,
-        number : newNumtoAdd
+        if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+          const nameToAdd = {
+            name : newName,
+            number : newNumtoAdd
+          }
+          const personId = personsTL.indexOf(newNameTL) + 1
+          noteService
+          .update(personId, nameToAdd)
+          .then(returnedPerson => {
+            setPersons(persons.map(person => person.id !== personId ? person : returnedPerson))
+            setErrorMessage(
+              `Added ${newName}`
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            setErrorMessage(
+              `Note '${newName}' was already removed from server`
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+          })  
         }
-        setPersons(persons.concat(nameToAdd))
-        setNewName('')
-        setNewNumber('')
+      } else {const nameToAdd = {
+          name : newName,
+          number : newNumtoAdd
+          }
+          noteService
+          .create(nameToAdd)
+          .then(returnedPerson => {
+            setPersons(persons.concat(returnedPerson))
+            setErrorMessage(
+              `Added ${newName}`
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            setErrorMessage(
+              `Information of${newName} has already been removed from server`
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+          })
       }
+  }
+
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)){
+      noteService
+        .remove(id)
+      
+      noteService
+      .getAll()
+      .then(initial => {
+        setPersons(initial)
+      })
+    } 
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter text={'filter shown with:'} value={filter} handleClick={handleFilterChange} />
       <h2>Add a new</h2>
-      <PersonForm name={newName} handleNameChange={handleNameChange} number={newNumber} handleNumberChange={handleNumberChange} addName={addName} />
+      <PersonForm 
+        name={newName} handleNameChange={handleNameChange} 
+        number={newNumber} handleNumberChange={handleNumberChange} 
+        addName={addName}
+      />
       <h2>Numbers</h2>
-      <Persons filter={filter} persons={persons} />
+      <Persons filter={filter} persons={persons} deletePerson={deletePerson} />
     </div>
   )
 
--- part2/puhelinluettelo/src/components/Persons.js
+++ part2/puhelinluettelo/src/components/Persons.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
-const filterFunc = ({ filter, persons }) => {
+const filterFunc = ({ filter, persons, deletePerson }) => {
     const copy = filter.toLowerCase()
-    const filtered = persons.map(person => person.name.toLowerCase().includes(copy) ? <p key={person.name}>{person.name}   {person.number}</p> : '')
+    const filtered = 
+    persons.map(person => 
+        person.name.toLowerCase().includes(copy) ? 
+            <div key={person.name}>
+                <p key={person.name}>{person.name}   {person.number}</p>
+                <button onClick={() => deletePerson(person.id, person.name)}>delete</button> 
+            </div>
+            : ''
+        )
     return filtered
   }
 
 const Persons = (props) => {
     return (
-        <div>{filterFunc(props)}</div>
+        <div>
+            {filterFunc(props)}
+        </div>
     )
 }
 
